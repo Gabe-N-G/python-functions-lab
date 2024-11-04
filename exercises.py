9e1b50905e753df20e9c745233e6d2e4c6b6152c
g g <gabedotgutierrez@gmail.com>
2024-11-04 17:34:56 -0500

    exercise 9 done, completely done

--- exercises.py
+++ exercises.py
@@ -127,3 +127,57 @@ def calculate_tip(bill,percent):
     return bill * percent/100
 
 print('Exercise 7:', calculate_tip(50, 20))
+
+# Exercise 8: Calculate Product of Numbers
+#
+# Write a function named `product` that takes an arbitrary number of numbers, multiplies them, and returns the product.
+# Review your notes on *args for handling an arbitrary number of arguments.
+#
+# Examples:
+# product(-1, 4) should return -4.
+# product(2, 5, 5) should return 50.
+#
+# Define the function and call it with different sets of numbers to test.
+
+def product(*args):
+    total = 1
+    for num in args:
+        total *= num
+    return total
+
+print('Exercise 8:', product(2, 5, 5))
+print('Exercise 8:', product(1, -4))
+
+# Exercise 9: Basic Calculator
+#
+# Create a function named `basicCalculator` that takes three arguments: 
+# two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide'). 
+# Perform the provided operation on the two numbers. In operations where the order of numbers is important, 
+# treat the first parameter as the first operand and the second parameter as the second operand.
+#
+# Examples:
+# basicCalculator(10, 5, 'subtract') should return 5.
+# basicCalculator(10, 5, 'add') should return 15.
+# basicCalculator(10, 5, 'multiply') should return 50.
+# basicCalculator(10, 5, 'divide') should return 2.
+#
+# Define the function and then call it below.
+
+#WHY IS THIS CAMELCASED!? BLASPHEMY
+def basicCalculator(x,y,op):
+    if(op=="subtract"):
+        return x-y
+    if(op=="add"):
+        return x+y
+    if(op=="multiply"):
+        return x*y
+    if(op=="divide"):
+        return x/y
+
+
+
+print('Exercise 9 Result:', basicCalculator(10, 5, "subtract"))
+print('Exercise 9 Result:', basicCalculator(10, 5, 'add'))
+print('Exercise 9 Result:', basicCalculator(10, 5, 'multiply'))
+print('Exercise 9 Result:', basicCalculator(10, 5, 'divide'))
+
